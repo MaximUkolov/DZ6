c4247038fcb5f57f603057b26e1b998d81787c86
Max <0665232@mail.ru>
2022-10-24 22:14:09 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+
+// Console.WriteLine("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+ 
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         Console.WriteLine($"Кол-во положительных: {count}");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Положительных чисел нет");
+//         break;
+//     }
+// }
+
+
+
+
+
+
+/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+
+Console.WriteLine("Введите b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1");
+int k1 = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine("Введите b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2");
+int k2 = Convert.ToInt32(Console.ReadLine());  
+int x = (b2 - b1) / (k1 - k2);
+int y = k1 * x + b1;
+Console.WriteLine("Координата пересечения прямых (x. y): " +(x, y));
+
+
+
+
+
+ 
+
+
+
+
+
+
